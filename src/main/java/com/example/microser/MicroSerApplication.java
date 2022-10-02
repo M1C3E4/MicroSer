96743af5f0174688f3205b260ee99931ff6b2dc4
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-10-02 16:18:50 +0100

    Initial commit

---
+++ src/main/java/com/example/microser/MicroSerApplication.java
@@ -0,0 +1,13 @@
+package com.example.microser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroSerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroSerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microser/MicroSerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroSerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
